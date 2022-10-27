d85488bfc3e4e61f7d2d9fbc237d0c217dcdba6a
Jonathan Delgado <113931071+jrdelgado3@users.noreply.github.com>
2022-10-27 17:43:37 +0000

    l

--- functions.py
+++ functions.py
@@ -1,8 +1,15 @@
+
+def add_numbers(num1, num2):
+  return num1 + num2
+def num_dividedFrom_add_numbers(num, addedNum):
+  return num/addedNum
+
 def function():
   pass
 #   Create Functions Practice #1
   # Declare a function called greet, which every time it is called prints "Hello world!"
-  
+def greet():
+  print("Hello world")
   # You should only define the function, you should not call it later.
 
 
@@ -11,6 +18,8 @@ def function():
 ########################################################################################################################
 # Create Functions Practice #2
 # Declare a function called welcome, which takes a person's name as an argument, and every time it is called, it prints "Welcome {name}!"
+def welcome(name):
+  print("welcome" + name)
 
 # Create the variable name, and store any name inside of it, so we can test the function with a name of your choosing.
 
@@ -21,7 +30,8 @@ def function():
 ########################################################################################################################
 # Create Functions Practice #3
 # Declare a function called square, that takes any number as an argument, and each time it is called, it prints the square of that number on the screen (that is, the value to the second power).
-
+def square(num):
+  return num **2
 # The name of the argument that this function must take is number. Create this variable and assign it any number.
 
 # You should only define the function and create the variable, you should not call the function afterwards.
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 # from methods_help import method_help
-from functions import function
+from functions import *
 # from returnStuff import returnS
 
 # method_help()
+welcome(" Jonathan")
 function()
+greet()
+
+added = add_numbers(45, 5)
+print(added)
+final = num_dividedFrom_add_numbers(45, added)
+print(final)
+square = square(30)
+print(square)
 # returnS()
